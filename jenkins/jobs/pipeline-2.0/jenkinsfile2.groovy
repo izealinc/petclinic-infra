a67ad0a090f695af864f29c2a0e1ec7a967bfd66
john <nehareddy1411@gmail.com>
2018-03-18 00:26:12 -0400

    Adding Jenkins2.groovy file

---
+++ jenkins/jobs/pipeline-2.0/jenkinsfile2.groovy
@@ -0,0 +1,36 @@
+
+pipeline {
+    agent any
+
+    stages {
+        stage ('Compile Stage') {
+
+            steps {
+                echo 'building petclinic project'
+                withMaven() {
+                    bat 'mvn clean compile'
+                }
+            }
+        }
+
+        stage ('Testing Stage') {
+
+            steps {
+                echo 'hello world'
+                withMaven() {
+                    bat 'mvn test'
+                }
+            }
+        }
+
+
+        stage ('Deployment Stage') {
+            steps {
+                echo 'hello world'
+                withMaven() {
+                    bat 'mvn deploy'
+                }
+            }
+        }
+    }
+}
